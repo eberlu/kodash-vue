4c29c7711813ae26370e2233a893c837a2c82dc6
Luciano Eberhardt <lucianoeberhardt95@outlook.com>
2023-08-14 21:48:03 -0300

    basic brand logo

---
+++ src/components/AppBrand.vue
@@ -0,0 +1,18 @@
+<template>
+
+	<svg 
+		width="163" 
+		height="157" 
+		viewBox="0 0 163 157" 
+		fill="none" 
+		xmlns="http://www.w3.org/2000/svg">
+
+		<path 
+			fill-rule="evenodd" 
+			clip-rule="evenodd" 
+			d="M0 0H163V157H132L84.5872 84.9149L129.734 17H90.3394L52.1651 77.3688V17H18V157H0V0ZM52.1651 95.6383L90.6881 157H52.1651V95.6383ZM132 99C143.598 99 153 89.598 153 78C153 66.402 143.598 57 132 57C120.402 57 111 66.402 111 78C111 89.598 120.402 99 132 99Z" 
+			fill="#EEF2FF" />
+
+	</svg>
+
+</template>
