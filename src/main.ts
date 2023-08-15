372129f8dd6f06f6e7da593781ac64b4adb2fb03
Luciano Eberhardt <lucianoeberhardt95@outlook.com>
2023-08-14 21:47:34 -0300

    add feather icons

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import DefaultLayout from './layouts/DefaultLayout.vue'
+import VueFeather from 'vue-feather'
+
 const app = createApp(App)
 
 app.use(router)
 
 app.component('DefaultLayout', DefaultLayout)
+app.component('FtIcon', VueFeather)
 
 app.mount('#app')
