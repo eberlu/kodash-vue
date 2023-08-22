d71ef5d89db682c3fc314a71fa42ca754e1f6f81
Luciano Eberhardt <lucianoeberhardt95@outlook.com>
2023-08-22 18:54:49 -0300

    nav fix and nav childs

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,11 @@
 export interface NavigationItem {
 	name: string;
-	to: string;
+	to?: string;
 	icon?: string;
+	children?: Array;
+}
+
+export interface NavigationChildItem {
+	name: string;
+	to: string;
 }
